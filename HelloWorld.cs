c5a974b620dc2aa39eba8d76cca1f38fe510c24e
pasha546 <125580422+pasha546@users.noreply.github.com>
2023-02-16 18:03:17 +0300

    Add files via upload
    тестовый скрипт Ярик негр

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод €рик негор
+        print("helloworld");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
